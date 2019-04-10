e19c6fe8c3ef5e1efbe90f547601472b3d356a15
nicobo <nicobo@users.noreply.github.com>
2019-04-10 21:45:54 +0200

    Scaffolded new ionic app

---
+++ src/app/tip/tip.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tip',
+  templateUrl: './tip.component.html',
+  styleUrls: ['./tip.component.scss'],
+})
+export class TipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
